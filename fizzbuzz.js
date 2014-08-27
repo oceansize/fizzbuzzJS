3034f9c92e66fb0f31a64dad2706d14f78ccfab6
Sarah C Young <sarahseewhy@gmail.com>
2014-08-27 10:31:44 +0100

    Tries a new approach to kata

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,3 +1,22 @@
-function disemvowel(str) {
-	return str.replace(/a|e|i|o|u/gi, "");
+var fizzBuzz = {
+	says : function(num) {
+
+		if (num % 15 == 0) {
+			return "fizzbuzz";
+		}
+
+		if (num % 3 == 0) {
+			return "fizz";
+		}
+
+		if (num % 5 == 0) {
+			return "buzz";
+		}
+
+		return num;
+	}
 }
+
+//The order in which I put the if statements matters because 15 is divisible by 3 & 5
+//and therefore I need to check for divisible 15 first before going through the other numbers.
+//Although a worthwhile excercise in practicng JS/Jasmine I think I'd like to try this kata a different way. 
--- spec/fizzbuzzSpec.js
+++ spec/fizzbuzzSpec.js
@@ -1,18 +1,34 @@
-describe("Disemvowler", function() {
+// describe 'class' or the thing I'm creating
+describe("Fizzbuzz", function() {
 
-	it("removes all lowercase vowels", function() {
-		expect(disemvowel("Hello world")).toEqual("Hll wrld");
+	//describe the method I want to test...
+	describe('says', function() {
+
+		//set expectations...
+		it('declares input number', function() {
+			//set 'actual' tentacle to hold the method (see Eloquent JS for explanation)
+			var actual = fizzBuzz.says(1);
+			expect(actual).toEqual(1);
+		});
+
+		it('declares "fizz" if the number is divisible by 3', function() {
+			//set 'actual' tentacle to hold method with new functionality/logic
+			//the logic is contained within the entire 'says' method
+			//as opposed to separating the logic into different methods
+			var actual = fizzBuzz.says(3);
+			expect(actual).toEqual("fizz");
+		});
+
+		it('declares "buzz" if the number is divisible by 5', function() {
+			//similar to above, set 'actual' to hold new functionality/logic
+			var actual = fizzBuzz.says(5);
+			expect(actual).toEqual("buzz");
+		})
+
+		it('declares "fizzbuzz" if the number is divisible by 15', function() {
+			var actual = fizzBuzz.says(15);
+			expect(actual).toEqual("fizzbuzz");
+		})
 	});
 
-	it("removes all uppercase vowels", function() {
-		expect(disemvowel("Artistic Eagle")).toEqual("rtstc gl");
-	});
-
-	it("shouldn't change empty strings", function() {
-		expect(disemvowel("")).toEqual("");
-	});
-
-	it("shouldn't change strings with no vowels", function() {
-		expect(disemvowel("Mhmm")).toEqual("Mhmm");
-	});
 });
